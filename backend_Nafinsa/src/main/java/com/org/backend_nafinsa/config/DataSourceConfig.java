fd5ad2c696f927c8f9007260ab128c8912e2bfd4
genaro <gcpcreative@gmail.com>
2022-12-08 11:50:11 -0600

    BACK- Configuracion de Data Source desde clase de Configuracion FRONT - Se agrega url con puerto 8080 para pruebas locales

--- backend_Nafinsa/src/main/java/com/org/backend_nafinsa/config/DataSourceConfig.java
+++ backend_Nafinsa/src/main/java/com/org/backend_nafinsa/config/DataSourceConfig.java
@@ -1,9 +1,14 @@
 package com.org.backend_nafinsa.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -20,7 +25,33 @@ public class DataSourceConfig {
         return (javax.sql.DataSource)context.lookup("jdbc/sicader");
     }
 
+    /*
+     * Configuracion de DS desde clase para prubas locales
+     */
+    
+    /*
+        @Bean(destroyMethod = "close")
+        DataSource dataSource(Environment env) {
+            HikariConfig config = new HikariConfig();
+            config.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+            config.setJdbcUrl("jdbc:oracle:thin:@172.16.8.113:1521:ocrl");
+            config.setUsername("system");
+            config.setPassword("N4f1n.0r4cl3DB");
+            return new HikariDataSource(config);
+        }
+     
+        @Bean
+        JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+     
+        @Bean
+        DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    */
 
+        
     //configuracion para conexion local o directa sin  ds
 /*
     @Bean
