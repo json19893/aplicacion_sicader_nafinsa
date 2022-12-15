36f1f35e167d584ee846e0cf25cf632c091b90df
totalplay <109114737+totalplay01@users.noreply.github.com>
2022-12-15 12:33:55 -0600

    conf

--- backend_Nafinsa/src/main/java/com/org/backend_nafinsa/config/DataSourceConfig.java
+++ backend_Nafinsa/src/main/java/com/org/backend_nafinsa/config/DataSourceConfig.java
@@ -32,7 +32,7 @@ public class DataSourceConfig {
 
      // Configuracion de DS desde clase para prubas locales
 
-        @Bean(destroyMethod = "close")
+       /* @Bean(destroyMethod = "close")
         DataSource dataSource(Environment env) {
             HikariConfig config = new HikariConfig();
             config.setDriverClassName("oracle.jdbc.driver.OracleDriver");
@@ -55,7 +55,7 @@ public class DataSourceConfig {
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
         return new JpaTransactionManager(emf);
-    }
+    }*/
 
     //configuracion para conexion local o directa sin  ds
 /*
